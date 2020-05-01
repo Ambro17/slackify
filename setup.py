d0d982708d64b4bf1318bea7258256af9ebed90a
Ambro17 <ambro17.1@gmail.com>
2020-05-01 15:11:08 -0300

    :wrench: Pin dependencies and fix setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,21 @@
+import sys
 from setuptools import setup
 
 with open('requirements.txt') as fp:
     install_requires = fp.read()
 
+if sys.version_info < (3, 6):
+    sys.exit('Sorry, Python < 3.6 is not supported')
+
 setup(
-    name='Flask-Slack',
+    name='slackify',
     version='0.0.1',
     url='https://github.com/Ambro17/Flask-Slack',
     author='Nahuel Ambrosini',
     author_email='ambro17.1@gmail.com',
-    description='Flask powered slack bots',
-    packages=['flask_slack'],
+    description='Pythonic API for modern slack bots',
+    packages=['slackify'],
     package_dir={'': 'src'},
     install_requires=install_requires,
+    python_requires='>=3.6'
 )
