fcda98d60f9d693f7eda24e24cc43eb49c319748
rdbreak <11014063+rdbreak@users.noreply.github.com>
2020-09-19 14:12:29 -0700

    Added examples (#22)
    * Added Home and Block Examples
    
    * Update .gitignore
    
    * Updated reply message
    
    Authored by @rdbreak

---
+++ examples/home_tab.py
@@ -0,0 +1,60 @@
+import os
+from flask import Flask
+from slackify import Slackify, request, reply_text, Slack, ACK
+
+# Initialize App
+app = Flask(__name__)
+slackify = Slackify(app=app)
+cli = Slack(os.getenv('SLACK_BOT_TOKEN'))
+
+
+@slackify.command
+def update_home_view():
+    user = request.form['user_id']
+    sample = {
+        "type": "home",
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "A simple stack of blocks for the simple sample Block Kit Home tab."
+                    }
+                },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "action_id": "tell_joke",
+                        "text": {
+                            "type": "plain_text",
+                            "text": "Tell me a Joke",
+                            "emoji": True
+                            }
+                        },
+                    ]
+                }
+            ]
+        }
+    resp = cli.views_publish(
+        user_id=user,
+        view=sample
+    )
+    assert resp.get('ok', False), f"Could not build home tab.\nError:\n{resp.data!r}"
+    return reply_text('Home tab updated.')
+
+
+@slackify.action('tell_joke')
+def tell_a_joke(action):
+    resp = cli.chat_postMessage(
+        channel=action['user']['id'],  # Send the user a private message through slackbot
+        text="I can't remember any joke right now 😅"
+    )
+    assert resp.get('ok'), f"Error sending message. {resp.data}"
+    return ACK
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=3000)
+
---
+++ examples/using_blocks.py
@@ -0,0 +1,46 @@
+import os
+from slackify import Slackify, Flask, Slack
+
+# Initialize App
+app = Flask(__name__)
+slackify = Slackify(app=app)
+cli = Slack(os.getenv('SLACK_BOT_TOKEN'))
+
+
+# Create greeting with formatted text response
+@slackify.message('hello')
+def say_hi(payload):
+    event = payload['event']
+    cli.chat_postMessage(
+        channel=event['channel'],
+        # Blocks can be used to insert markdown support for links, images, and more. Check out https://api.slack.com/reference/block-kit for more.
+        blocks=[
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Danny Torrence left the following review for your property:"
+                    }
+                },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "<https://example.com|Overlook Hotel> \n :star:\n Doors had too many axe holes, guest in room " + "237 was far too rowdy, whole place felt stuck in the 1920s."
+                    },
+                "accessory": {
+                    "type": "image",
+                    "image_url": "https://images.pexels.com/photos/750319/pexels-photo-750319.jpeg", "alt_text": "Haunted hotel image"
+                    }
+                },
+            {
+                "type": "section",
+                "fields": [
+                    {
+                        "type": "mrkdwn",
+                        "text": "*Average Rating*\n1.0"
+                        }
+                    ]
+                }
+            ]
+        )
